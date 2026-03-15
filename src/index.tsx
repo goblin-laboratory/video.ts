c079196c6625432b54d9833a7ae0c8187b87bbcf
xiaosong <kunkkaco@gmail.com>
2026-03-15 13:06:13 +0800

    feat: 在入口文件中添加 antd ConfigProvider 配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
 import App from './App';
 
 const rootEl = document.getElementById('root');
@@ -7,7 +8,9 @@ if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <ConfigProvider>
+        <App />
+      </ConfigProvider>
     </React.StrictMode>,
   );
 }
